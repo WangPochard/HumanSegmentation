1a11cc23e932851511df1a27aa5501b570f0a8ae
WangPochard <40340106@gm.nfu.edu.tw>
2023-05-17 22:19:58 +0800

    update code

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -30,24 +30,49 @@ def MaskedImage_Save(src_img, src_JsonParam, path):
     :return: message (Failed or Success)
     """
 
+
+    # resize shape
+    widgh = 1000
+    height = 1000
     try:
         df_shape = pd.DataFrame.from_dict(src_JsonParam["shapes"].iloc[0])
         arr_pts = np.array(df_shape["points"].values[0], np.int32)
-
-    # Color轉換成tuple形式之後還是沒辦法傳入cv2的func裡 (無論是 polylines or fillPoly 都不行) ↓
-                                                            # arr_lineColor = np.array(df["lineColor"].iloc[0], np.int32)
-                                                            # arr_fileColor = np.array(df["fillColor"].iloc[0], np.int32)
-    # Create 一個 value為0的 img_mat
+        # Color轉換成tuple形式之後還是沒辦法傳入cv2的func裡 (無論是 polylines or fillPoly 都不行) ↓
+        # arr_lineColor = np.array(df["lineColor"].iloc[0], np.int32)
+        # arr_fileColor = np.array(df["fillColor"].iloc[0], np.int32)
+        # Create 一個 value為0的 img_mat
         img_zero = np.zeros(src_img.shape, dtype=np.uint8)
-        image = cv2.fillPoly(img_zero, [arr_pts], color=(0, 255, 0))
-
+        masked_img = cv2.fillPoly(img_zero, [arr_pts], color=(0, 255, 0))
         fileName = str(src_JsonParam["imagePath"].iloc[0])
+        # print(fileName)
 # Processing Steps
     # 1. resize image
     # 2. up sampling (方向轉換(平移)、隨機翻轉、旋轉)
+        Src_images = dict()
+        src_img = cv2.resize(src_img, (widgh, height))
+        Src_images.update({f"0_{fileName}": src_img})
+        Src_images.update({f"1_{fileName}": cv2.flip(src_img, 1)}) # 水平翻轉
+
+        Mask_images = dict()
+        masked_img = cv2.resize(masked_img, (widgh, height))
+        Mask_images.update({f"0_{fileName}": masked_img})
+        Mask_images.update({f"1_{fileName}": cv2.flip(masked_img, 1)}) # 水平翻轉
+        # Save image
+        for ImgName in Src_images.keys():
+            # print(f"{path}src/{ImgName}")
+            # print(f"FileName:\t{ImgName}")
+            SrcImg = Src_images[ImgName]
+            # print(type(SrcImg))
+            cv2.imwrite(f"{path}src/{ImgName}", SrcImg)  # resize source image
+        for ImgName in Mask_images.keys():
+            # print(f"{path}src/{ImgName}")
+            MaskImg = Mask_images[ImgName]
+            cv2.imwrite(f"{path}masked/{ImgName}", MaskImg)  # resize masked image
+
     # 3. 資料集切割(分另一個.py檔處理)
-        cv2.imwrite() # resize source image
-        cv2.imwrite(f"{path}/masked/{fileName}",image) # resize masked image
+
+        # cv2.imwrite(f"{path}/src/{fileName}",src_image) # resize source image
+        # cv2.imwrite(f"{path}/masked/{fileName}",masked_image) # resize masked image
 
         msg = "Success!!!"
     except:
@@ -57,6 +82,7 @@ def MaskedImage_Save(src_img, src_JsonParam, path):
 
 if __name__ == "__main__":
     path = f"{os.getcwd()}/dataset/"
+    process_path = f"{os.getcwd()}/resize_dataset/"
     files_path = get_json_files(path)
     for file_path in files_path:
         print(file_path)
@@ -65,7 +91,7 @@ if __name__ == "__main__":
         df_json_detail = pd.json_normalize(json_detail)  # JsonParam : json_detail
         print(df_json_detail)
         img = cv2.imread(file_path.replace(".json", ".png"))
-        msg = MaskedImage_Save(img, df_json_detail, path)
+        msg = MaskedImage_Save(img, df_json_detail, process_path)
         print(msg)
 # print(df)
 # print(df.columns)
